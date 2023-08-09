b1fe556d4837d090d7bb312fe428cd4f1e930421
Bahar Aydogdu <bahar.aydogdu@siemens.com>
2023-08-09 16:22:23 +0300

    filter unit test updated

--- src/test/java/com/booking/filter/BookingFilterTest.java
+++ src/test/java/com/booking/filter/BookingFilterTest.java
@@ -1,6 +1,7 @@
 package com.booking.filter;
 
 import com.booking.entity.model.BookingRequest;
+import com.booking.exception.BookingApplicationException;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.testng.annotations.BeforeMethod;
@@ -16,29 +17,44 @@ public class BookingFilterTest {
     @InjectMocks
     private BookingFilter bookingFilter = new BookingFilter();
     
-    private BookingRequest validRequest1;
-    private BookingRequest validRequest2;
-    private BookingRequest invalidRequest;
-    List<BookingRequest> requests = new ArrayList<>();
     
     @BeforeMethod
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        validRequest1 = new BookingRequest("0900 1700", "2020-01-18 10:17:06", "EMP001", "2020-01-21 09:00", "2");
-        validRequest2 = new BookingRequest("0900 1700", "2020-01-18 12:34:56", "EMP002", "2020-01-21 09:00", "2");
-        invalidRequest = new BookingRequest("0900 1700", "2020-01-15 17:29:12", "EMP003", "2020-01-21 16:30", "1");
-        requests.add(validRequest1);
-        requests.add(invalidRequest);
-        requests.add(validRequest2);
     }
     
     @Test
-    public void shouldFilterInvalidRequests() {
+    public void shouldFilterOfficeHoursExceededRequests() {
  
-        List<BookingRequest> validRequests = bookingFilter.filterInvalidRequests(requests);
+        List<BookingRequest> filteredRequests = bookingFilter.filterInvalidRequests(createRequestList());
         
-        assertEquals(1, validRequests.size());
-        assertEquals(validRequest1, validRequests.get(0));
+        assertEquals(1, filteredRequests.size());
+        assertEquals(filteredRequests.get(0).getEmployeeId(), "EMP001");
     }
     
+    @Test
+    public void shouldFilterOverlappedRequests() throws BookingApplicationException {
+        
+        List<BookingRequest> filteredRequests = bookingFilter.filterOverlappedRequests(createOverlappedRequestList());
+        
+        assertEquals(2, filteredRequests.size());
+        assertEquals(filteredRequests.get(0).getEmployeeId(), "EMP001");
+        assertEquals(filteredRequests.get(1).getEmployeeId(), "EMP003");
+    }
+    
+    private List<BookingRequest> createRequestList() {
+        List<BookingRequest> requests = new ArrayList<>();
+        requests.add(new BookingRequest("0900 1700", "2020-01-18 10:17:06", "EMP001", "2020-01-21 09:00", "2"));
+        requests.add(new BookingRequest("0900 1700", "2020-01-18 12:34:56", "EMP002", "2020-01-21 08:00", "2"));
+        requests.add(new BookingRequest("0900 1700", "2020-01-15 17:29:12", "EMP003", "2020-01-21 16:30", "1"));
+        return requests;
+    }
+    
+    private List<BookingRequest> createOverlappedRequestList() {
+        List<BookingRequest> requests = new ArrayList<>();
+        requests.add(new BookingRequest("0900 1700", "2020-01-18 10:17:06", "EMP001", "2020-01-21 09:00", "2"));
+        requests.add(new BookingRequest("0900 1700", "2020-01-18 12:34:56", "EMP002", "2020-01-21 09:00", "2"));
+        requests.add(new BookingRequest("0900 1700", "2020-01-15 17:29:12", "EMP003", "2020-01-21 14:30", "1"));
+        return requests;
+    }
 }
